0f166245dc8d0c13e9350bdbc5449ce9c1617995
Molka206 <129753559+Molka206@users.noreply.github.com>
2024-04-28 22:26:56 +0100

    Update send.php

--- View/send.php
+++ View/send.php
@@ -13,7 +13,7 @@ if (isset($_POST["emailrep"])) {
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'molkas206@gmail.com';
-    $mail->Password = 'lqumwnqmggsdujvw';
+    $mail->Password = '';
    // $mail->SMTPSecure = 'tls';
    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
     $mail->Port = 465;
